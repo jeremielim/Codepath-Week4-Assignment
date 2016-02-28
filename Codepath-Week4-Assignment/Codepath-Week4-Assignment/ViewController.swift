894850fb8863da447486f08792ef3246f029506a
Jeremie Lim <jeremielim@Jeremies-MBP.attlocal.net>
2016-02-28 13:20:05 -0800

    Added initial screens

---
+++ Codepath-Week4-Assignment/Codepath-Week4-Assignment/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Codepath-Week4-Assignment
+//
+//  Created by Jeremie Lim on 2/28/16.
+//  Copyright © 2016 Jeremie Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
