20cf54d903e4ec1f5031babd3c0740b1a148a7a2
Jeremie Lim <jeremielim@Jeremies-MBP.attlocal.net>
2016-02-28 22:42:03 -0800

    added search delays

--- Codepath-Week4-Assignment/Codepath-Week4-Assignment/SearchViewController.swift
+++ Codepath-Week4-Assignment/Codepath-Week4-Assignment/SearchViewController.swift
@@ -12,12 +12,19 @@ class SearchViewController: UIViewController {
 
     @IBOutlet weak var searchFeed: UIImageView!
     
+    var loadImagesBackground: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        loadImagesBackground = UIView()
         
-        let loadImagesBackground: UIView = UIView()
         loadImagesBackground.frame = CGRect(x: 0, y: 0, width: 320, height: 524)
+        
         loadImagesBackground.backgroundColor = UIColor(red: 53.0/255.0, green: 66.0/255.0, blue: 84.0/255.0, alpha: 1)
         view.addSubview(loadImagesBackground)
         
@@ -34,18 +41,15 @@ class SearchViewController: UIViewController {
         loader.animationDuration = 0.3
         loader.startAnimating()
         loadImagesBackground.addSubview(loader)
-        
-        searchFeed.alpha = 0
-        
-        delay(0.8) { () -> () in
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        delay(1) { () -> () in
             UIView.animateWithDuration(0.3, animations: { () -> Void in
-                loadImagesBackground.alpha = 0
+                self.loadImagesBackground.alpha = 0
                 self.searchFeed.alpha = 1
             })
         }
-        
-        
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
